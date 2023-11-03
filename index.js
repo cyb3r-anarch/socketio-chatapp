cebd6d69e566cbcd061aa428a208825899d53795
cyb3r-anarch <aanon408@gmail.com>
2023-11-03 01:38:38 -0700

    added a socket.io instance that listens to incoming websockets and executes a callback function

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 //import the express library
 //which is a nodejs framework used to handle webserver and http middleware
 import express from 'express';
-
 //import createServer function from node.js native HTTP module
 import {createServer} from 'node:http';
 //The createServer function returns a new HTTP server instance
 //that can be used to handle HTTP requests and responses
+
+//import Server class from socket.io library
+//it's used to manage Websocket servers
+import {Server} from "socket.io";
 
 //create an instance of the express application
 //app is a function handler for express()
 const app = express();
 //initiate and assign a variable for the HTTP server port
 const port = 3000;
-
 //create an HTTP Server using the createServer function and pass the express app as it's handler
 //everytime the server gets a request, the requestListener - app is executed
 const server = createServer(app);
+//initalize a new Websocket server instance from Server class
+const io = new Server(server);
 
 //define a route that responds to HTTP GET requests at the root URL ('/')
 app.get('/' , (req,res)=>{
@@ -25,6 +29,10 @@ app.get('/' , (req,res)=>{
   res.sendFile(new URL('./index.html', import.meta.url).pathname)
 });
 
+//Listens for a Websocket connection and executes a callback function
+io.on('connection', (socket) => {
+  console.log("a user has connected");
+})
 //Start HTTP Server and have it listen at {port}
 server.listen(port, ()=>{
   console.log('server running at port 3000');
