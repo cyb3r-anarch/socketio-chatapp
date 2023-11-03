9ff5a4ae1c422a8e15f7387a7f78f44f42a25321
cyb3r-anarch <aanon408@gmail.com>
2023-11-02 22:55:21 -0700

    In this initial commit, I've set up a basic web server using the Express framework and the native Node.js HTTP module. Express is a popular framework for building web applications and APIs, while the use of the native HTTP module provides low-level control over HTTP request handling.
    I've also defined a simple route that responds to HTTP GET requests at the root URL ('/'). This approach allows me to have fine-grained control over how my server handles HTTP requests while benefiting from Express routing and middleware capabilities.
    
    Additionally, I've added comments to explain the code's purpose and my choice of using the native HTTP module over the Express one for low-level control.
    
    This commit serves as the starting point for the project

---
+++ index.js
@@ -0,0 +1,37 @@
+//import the express library
+//which is a nodejs framework used to handle webserver and http middleware
+import express from 'express';
+
+//import createServer function from node.js native HTTP module
+import {createServer} from 'node:http';
+//The createServer function returns a new HTTP server instance
+//that can be used to handle HTTP requests and responses
+
+//create an instance of the express application
+//app is a function handler for express()
+const app = express();
+//initiate and assign a variable for the HTTP server port
+const port = 3000;
+
+//create an HTTP Server using the createServer function and pass the express app as it's handler
+//everytime the server gets a request, the requestListener - app is executed
+const server = createServer(app);
+
+//define a route that responds to HTTP GET requests at the root URL ('/')
+app.get('/' , (req,res)=>{
+  res.send("<h1>Hello World</h1>"); 
+});
+
+//Start HTTP Server and have it listen at {port}
+server.listen(port, ()=>{
+  console.log('server running at port 3000');
+});
+
+/* we use the native node http module over the express one 
+ * because it gives us more low level control when it comes 
+ * to low-level apis provided by nodejs when handling http requests.*/
+
+
+/* the approach above lets me keep fine grain control over how my Server
+ * handles HTTP requests while benefitting from express routing and middleware
+ * capabilities*/
